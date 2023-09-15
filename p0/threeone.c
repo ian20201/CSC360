2347bd45ae8a3507388eb0aeda389df6830ba499
yulunchen <yulunchen@uvic.ca>
2023-09-14 21:51:40 -0700

    First Push

---
+++ p0/threeone.c
@@ -0,0 +1,28 @@
+//Name : Ian Chen
+//V-Number : V00887293
+//Date : 2023-09-14
+//CSC 360 Assignment 1
+
+int main(int argc, char*argv[]){
+
+}
+
+void getcwd(){
+
+}
+
+void getlogin(){
+
+}
+
+void getusername(){
+
+}
+
+void fork(){
+
+}
+
+void execvp(){
+    
+}
