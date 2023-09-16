639a6c0c4d2bf1ce771b8b5caa905b3f38778ce4
yulunchen <yulunchen@uvic.ca>
2023-09-16 12:01:50 -0700

    Clean the code

--- p0/threeone.c
+++ p0/threeone.c
@@ -61,18 +61,6 @@ void getinput(char* input){
     }
 }
 
-char get_directory(){
-
-}
-
-char get_username(){
-
-}
-
-char get_hostname(){
-    
-}
-
 int use_fork(char split[MAX_IN_COMMAND][MAX_IN_CHARS],int args){
     pid_t pid;
     char* command = split[0];
@@ -83,33 +71,7 @@ int use_fork(char split[MAX_IN_COMMAND][MAX_IN_CHARS],int args){
         strcpy(argument_list[counter],split[counter]);
         //Save all the command into the argument_list
     }
-    argument_list[args] = NULL;
-
-    // pid_t pid;
-    // time_t t;
-    // int status;
-
-    // if ((pid = fork()) < 0)
-    //     perror("fork() error");
-    // else if (pid == 0) {
-    //     int status_code = execvp(command, argument_list);
-    //     exit(1);
-    // }
-    // else do {
-    //     if ((pid = waitpid(pid, &status, WNOHANG)) == -1)
-    //     perror("wait() error");
-    //     else if (pid == 0) {
-    //     // time(&t);
-    //     // printf("child is still running at %s", ctime(&t));
-    //     // sleep(1);
-    //     }
-    //     else {
-    //     if (WIFEXITED(status))
-    //         printf("child exited with status of %d\n", WEXITSTATUS(status));
-    //     else puts("child did not exit successfully");
-    //     }
-    // } while (pid == 0);
-    
+    argument_list[args] = NULL;  
 
     int status;
     pid = fork();
@@ -118,12 +80,8 @@ int use_fork(char split[MAX_IN_COMMAND][MAX_IN_CHARS],int args){
         perror("fork");
         exit(EXIT_FAILURE);
     case 0:{
-            // puts("Child exiting.");
-            // exit(EXIT_SUCCESS);
-            // Newly spawned child Process. This will be taken over by "ls -l"
             int status_code = execvp(command, argument_list);
-            // printf("ls -l has taken control of this child process. This won't execute unless it terminates abnormally!\n");
-            exit(1);
+            // exit(1);
             if (status_code == -1) {
                 printf("Terminated Incorrectly\n");
                 return 1;
@@ -133,14 +91,13 @@ int use_fork(char split[MAX_IN_COMMAND][MAX_IN_CHARS],int args){
         do {
             if ((pid = waitpid(pid, &status, WNOHANG)) == -1)
                 perror("wait() error");
-            else if (pid == 0) {
-                // time(&t);
-                // printf("child is still running at %s", ctime(&t));
-                // sleep(1);
-            }
+            // else if (pid == 0) {
+            //     time(&t);
+            //     printf("child is still running at %s", ctime(&t));
+            //     sleep(1);
+            // }
             else {
                 if (WIFEXITED(status));
-                    // printf("child exited with status of %d\n", WEXITSTATUS(status));
                 else puts("child did not exit successfully");
             }
         } while (pid == 0);        
@@ -155,7 +112,7 @@ int use_fork(char split[MAX_IN_COMMAND][MAX_IN_CHARS],int args){
 int main(int argc, char*argv[]){
 
     char* input;
-    char* prompt = {};
+    // char* prompt = [];
 
     while ((input = readline("> ")) != NULL) {
         if (strlen(input) > 0) {
